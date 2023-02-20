d98ec69a21e2f8db2dab9a963b7a964749194706
HotJuly <949588380@qq.com>
2023-02-20 11:56:17 +0800

    66.对key值的理解

--- 02.代码/03.vue2_api/src/App.vue
+++ 02.代码/03.vue2_api/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
+     -->
+     <ul>
+      <li v-for="(item,index) in arr" :key="index">
+        <label>{{ item }}</label>
+        <input type="text">
+      </li>
+     </ul>
+     <button @click="handler">修改数据</button>
   </div>
 </template>
 
@@ -11,24 +19,30 @@ export default {
   name: 'App',
   data(){
     return{
-      msg:123
+      msg:123,
+      arr:[1,2,3,4,5]
+    }
+  },
+  methods:{
+    handler(){
+      this.arr.splice(1,0,7);
     }
   },
   components: {
     HelloWorld
   },
-  beforeCreate(){
-    console.log('--------beforeCreate--------',this,this.$data,this.msg,this.$el)
-  },
-  created(){
-    console.log('--------created--------',this,this.$data,this.msg,this.$el)
-  },
-  beforeMount(){
-    console.log('--------beforeMount--------',this,this.$data,this.msg,this.$el)
-  },
-  mounted(){
-    console.log('--------mounted--------',this,this.$data,this.msg,this.$el)
-  }
+  // beforeCreate(){
+  //   console.log('--------beforeCreate--------',this,this.$data,this.msg,this.$el)
+  // },
+  // created(){
+  //   console.log('--------created--------',this,this.$data,this.msg,this.$el)
+  // },
+  // beforeMount(){
+  //   console.log('--------beforeMount--------',this,this.$data,this.msg,this.$el)
+  // },
+  // mounted(){
+  //   console.log('--------mounted--------',this,this.$data,this.msg,this.$el)
+  // }
 }
 </script>
 
